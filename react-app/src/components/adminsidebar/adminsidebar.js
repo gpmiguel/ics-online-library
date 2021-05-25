b952bede1f838d6054cd168b488a4363eee157f4
jclaput <jclaput@up.edu.ph>
2021-05-26 03:35:07 +0800

    fixed admin sidebar layout

--- react-app/src/components/adminsidebar/adminsidebar.js
+++ react-app/src/components/adminsidebar/adminsidebar.js
@@ -7,16 +7,16 @@ class AdminSidebar extends Component{
         return(
             <div>
                 <p className="subhead-text" style={{fontSize: "18px"}}>Manage Resources</p>
-                <p><Link to ="/add-book"> <button type="button" className="btn btn-primary btn-block btn-md ">Add Book</button></Link></p>
-                <p><Link to ="/add-academic-paper"><button type="button" className="btn btn-primary btn-block btn-md">Add Academic Paper</button></Link></p>
+                <p><Link to ="/add-book"> <button type="button" className="btn btn-primary btn-block btn-md mb-3">Add Book</button></Link></p>
+                <p><Link to ="/add-academic-paper"><button type="button" className="btn btn-primary btn-block btn-md mb-3">Add Academic Paper</button></Link></p>
 
                 <p className="subhead-text" style={{fontSize: "18px"}}>Manage Faculty and Staff</p>
-                <Link to ="/add-academic-paper"><button type="button" className="btn btn-primary btn-md">Add Faculty and Staff</button></Link>
-                <Link to ="/"><button type="button" className="btn btn-primary btn-md">Edit Faculty and Staff</button></Link>
+                <Link to ="/add-academic-paper"><button type="button" className="btn btn-primary btn-block btn-md mb-3">Add Faculty and Staff</button></Link>
+                <Link to ="/"><button type="button" className="btn btn-primary btn-block btn-md mb-3">Edit Faculty and Staff</button></Link>
 
                 <p className="subhead-text" style={{fontSize: "18px"}}>Generate Reports</p>
-                <button type="button" className="btn btn-primary btn-block btn-md">Summary Report</button>
-                <button type="button" className="btn btn-primary btn-block btn-md">Activity Report</button>
+                <button type="button" className="btn btn-primary btn-block btn-md mb-3">Summary Report</button>
+                <button type="button" className="btn btn-primary btn-block btn-md mb-3">Activity Report</button>
             </div>
         );
     }
