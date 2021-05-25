d490db2b7bba329ebb1edc11b31f4d4876cdf60a
John Conrad M. Laxamana <jmlaxamana@up.edu.ph>
2021-05-25 19:52:32 +0800

    submitting form now clears the fields

--- react-app/src/components/add-book/add-book.js
+++ react-app/src/components/add-book/add-book.js
@@ -40,14 +40,14 @@ const AddBook = () =>{
 
         //uncomment if author and subject fields' TagsInput issue is resolved
 
-        // if(!bookData.author){
-        //     alert('Please add the Author.');
-        //     return;
-        // }
-        // if(!bookData.subject){
-        //     alert('Please add the Subject.');
-        //     return;
-        // }
+        if(!bookData.author){
+            alert('Please add the Author.');
+            return;
+        }
+        if(!bookData.subject){
+            alert('Please add the Subject.');
+            return;
+        }
         if(!bookData.yearPublished){
             alert('Please add the Year Published.');
             return;
@@ -114,49 +114,49 @@ const AddBook = () =>{
                                 <p className="text-center yellow-title-header mt-3 mb-1 head-text" style={{fontSize: "48px"}}>Add Book</p>
                                 
                                 <label for="bookTitleFormInput" className="form-label">Title</label>
-                                <input type="text" name="title" onChange={onChange} className="form-control" id="bookTitleFormInput"/>
+                                <input type="text" value={bookData.title} name="title" onChange={onChange} className="form-control" id="bookTitleFormInput"/>
                                 {/* ask about why tagsinput */}
-                                <label className="form-label mt-3">Author</label>
+{/*                                <label className="form-label mt-3">Author</label>
                                 <TagsInput/>
 
                                 <label className="form-label mt-3">Subject</label>
-                                <TagsInput/>                                
-{/*                                <label className="form-label mt-3">Author</label>
-                                <input type="text" name="author" onChange={onChange} className="form-control" id="bookTitleFormInput"/>
+                                <TagsInput/>*/}                                
+                                <label className="form-label mt-3">Author</label>
+                                <input type="text" value={bookData.author} name="author" onChange={onChange} className="form-control" id="bookAuthorFormInput"/>
 
                                 <label className="form-label mt-3">Subject</label>
-                                <input type="text" name="subject" onChange={onChange} className="form-control" id="bookTitleFormInput"/>
-*/}
+                                <input type="text" value={bookData.subject} name="subject" onChange={onChange} className="form-control" id="bookSubjectFormInput"/>
+
                                 <label for="yearpublishedFormInput" className="form-label mt-3">Year Published</label>
-                                <input type="text" name="yearPublished" onChange={onChange} className="form-control" id="yearpublishedFormInput"/>
+                                <input type="text" value={bookData.yearPublished} name="yearPublished" onChange={onChange} className="form-control" id="yearpublishedFormInput"/>
                                 
                                 <label for="pagecountFormInput" className="form-label mt-3">Page Count</label>
-                                <input type="text" name="pageCount" onChange={onChange} className="form-control" id="pagecountFormInput"/>
+                                <input type="text" value={bookData.pageCount} name="pageCount" onChange={onChange} className="form-control" id="pagecountFormInput"/>
 
                                 <label for="publisherFormInput" className="form-label mt-3">Publisher</label>
-                                <input type="text" name="publisher" onChange={onChange} className="form-control" id="publisherFormInput"/>
+                                <input type="text" value={bookData.publisher} name="publisher" onChange={onChange} className="form-control" id="publisherFormInput"/>
 
                                 <label for="editionFormInput" className="form-label mt-3">Edition</label>
-                                <input type="text" name="edition" onChange={onChange} className="form-control" id="editionFormInput"/>
+                                <input type="text" value={bookData.edition} name="edition" onChange={onChange} className="form-control" id="editionFormInput"/>
 
                                 <label for="isbnFormInput" className="form-label mt-3">ISBN</label>
-                                <input type="text" name="isbn" onChange={onChange} className="form-control" id="isbnFormInput"/>
+                                <input type="text" value={bookData.isbn} name="isbn" onChange={onChange} className="form-control" id="isbnFormInput"/>
 
                                 <label for="introductionFormInput" className="form-label mt-3">Introduction</label>
-                                <textarea name="introduction" onChange={onChange} className="form-control" id="introductionFormInput" rows="6"></textarea>                               
+                                <textarea value={bookData.introduction} name="introduction" onChange={onChange} className="form-control" id="introductionFormInput" rows="6"></textarea>                               
 
                                 {/* file uploads */}
                                 <div className="row mt-3">
                                     <div className="col-3">
                                         <label for="maincopyFormFile" className="form-label">Main Copy</label>
-                                        <input name="mainCopy" onChange={onChange} className="form-control" type="file" id="maincopy"/>
+                                        <input value={bookData.mainCopy} name="mainCopy" onChange={onChange} className="form-control" type="file" id="maincopy"/>
                                     </div>
                                 </div>
 
                                 <div className="row mt-3 mb-5">
                                     <div className="col-3">
                                         <label for="resourceImageFormFile" className="form-label">Resource Display Image</label>
-                                        <input name="resourceImage" onChange={onChange} className="form-control" type="file" id="resourceImageFormFile"/>
+                                        <input value={bookData.resourceImage} name="resourceImage" onChange={onChange} className="form-control" type="file" id="resourceImageFormFile"/>
                                     </div>
                                 </div>
                                 {/*<h1>{JSON.stringify(bookData)}</h1>*/}
