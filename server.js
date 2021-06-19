f46a3433ca0a7962aab8c6e34e3ef45ac852fd35
gpmiguel <gpmiguel@up.edu.ph>
2021-06-19 23:53:49 +0800

    trial

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 
-const path = require('path')
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'frontend/public')));
+const port = process.env.PORT || 3001;
 
 mongoose.connect("mongodb+srv://c4ladmin:icsadmin@icslibrarysystem.5tt8e.mongodb.net/icslibrarysystem", 
     {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }
@@ -18,13 +17,7 @@ connection.once('open', ()=> {
     console.log("MongoDB database connection is established.");
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/public/index.html'));
-});
-
-app.use("/", require("./backend/routes/route.js"));
-
-const port = process.env.PORT || 3001;
+app.use("/", require("./backend/routes/route"));
 
 app.listen(port, ()=> {
     console.log(`Server is running in port: ${port} 🔥`);
