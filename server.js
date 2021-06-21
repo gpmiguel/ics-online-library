af90610da9ec09d5f0a711a95fc6c7dbe3302375
gpmiguel <gpmiguel@up.edu.ph>
2021-06-21 15:24:33 +0800

    trial

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
